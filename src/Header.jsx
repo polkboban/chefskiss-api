9f3badcaaaaff45ee6e5090dc72312dc8ca7e9ce
Paul Kuriakose <polkboban@gmail.com>
2025-03-18 19:31:11 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return(
         <header>
             <img src="/chefhat.png" className="chef-logo" />
-            <h1>Chef Claude</h1>
+            <h1>Chef's Kiss</h1>
         </header>
     )
 }
